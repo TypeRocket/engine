7f4b760f1a71af3ff7e8148460fe8cb1469152e3
Kevin Dees <kvdees@gmail.com>
2023-12-15 15:59:10 -0500

    Add SqlRaw class
    A new SqlRaw class was added. This class primarily serves for direct interaction with SQL through string representation.

---
+++ src/Database/SqlRaw.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TypeRocket\Engine7\Database;
+
+class SqlRaw
+{
+    protected string $sql = '';
+
+    public function __construct(string $sql)
+    {
+        $this->sql = $sql;
+    }
+
+    public function __toString()
+    {
+        return $this->sql;
+    }
+
+    public static function new(...$args) : static
+    {
+        return new static(...$args);
+    }
+}
